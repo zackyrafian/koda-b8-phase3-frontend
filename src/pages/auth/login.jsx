c25e85b7274d48984d7b581aabb3f5b30e5afcfa
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-18 14:41:10 +0700

    feat: add login page

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+const API = "http://localhost:2020"
+
+
+export default function LoginPage() { 
+
+  const handleSubmit = async (e) => { 
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target)) 
+    console.log(data)
+    try { 
+      const res = await fetch(`${API}/auth/login`, { 
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+      })
+      if (!res.ok) { 
+        throw new Error("Failed fetch data.")
+      }
+      const responseData = await res.json();
+      localStorage.setItem("token", responseData.results.token)
+    } catch (e) { 
+      console.log(e.message)
+    }
+  }
+  return ( 
+    <div className="flex min-h-screen items-center justify-center flex-col gap-4">
+      <div>Login</div>
+
+      <form onSubmit={handleSubmit} action="" className="flex gap-2 flex-col min-w-1/4"> 
+        <div className="flex flex-col">
+          <label htmlFor="">Email</label>
+          <input name="email" className="px-4 py-2 border rounded-xl" type="email" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="">Password</label>
+          <input name="password" className="px-4 py-2 border rounded-xl" type="password" />
+        </div>
+        <button className="flex items-center justify-center">Sign Up</button>
+      </form>
+      <div>
+        <span>Already have an account? <Link to={"/login"}>Register</Link></span>
+      </div>
+    </div>
+  )
+}
