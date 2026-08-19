c3fc13b37ad9a8f5be6b767d354f2a4a749f3bcc
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-19 16:22:36 +0700

    feat: add page dashboard my-links

---
+++ src/pages/dahsboard/my-link.jsx
@@ -0,0 +1,100 @@
+import { Copy, Link2, ListFilter, Search, Trash } from "lucide-react";
+import { useContext, useEffect, useState, } from "react";
+import Header from "../../components/common/header";
+import { AuthContext } from "../../context/authContext";
+const API = "http://localhost:2020"
+
+export default function MyLinksPage() { 
+  const { token } = useContext(AuthContext);
+  const [slugs, setSlugs] = useState(); 
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(async() => { 
+      (async () => { 
+        try {
+          const res = await fetch(`${API}/links?search=${encodeURIComponent(query)}`, {
+            headers: {
+              "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+          })
+  
+          if (!res.ok) { 
+            throw new Error ("Failed fetch")
+          }
+          const data = await res.json();
+          setSlugs(data.results)
+        } catch (e) { 
+          console.log(e.message)
+        }
+      })()
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [query])
+
+  const handleRemove = async (id) => { 
+    try { 
+      const res = await fetch(`${API}/links/${id}`, { 
+        method: "DELETE", 
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      if (!res.ok) { 
+        throw new Error (res.message)
+      }
+      setSlugs((prev) => prev.filter((slug) => slug.id!== id))
+    } catch (error) { 
+      console.error(error.message)
+    }
+  }
+  return ( 
+    <div className="flex flex-col gap-8">
+      <Header/>
+      <div className="flex flex-col items-center min-w-screen min-h-screen gap-4">
+        <div className="min-w-2xl flex-col gap-4 flex">
+          <div className="flex justify-between">
+            <div>
+              <h1 className="text-3xl font-semibold">My Links</h1>
+              <p>Manage and track your shortened digital assets.</p>
+            </div>
+            <div className="text-right">
+              <h1 className="text-xl">TOTAL ACTIVE</h1>
+              <p className="text-3xl font-semibold ">124</p>
+            </div>
+          </div>
+          <div className="bg-white p-2 rounded-lg flex gap-3 justify-between border border-black/20">
+            <div className="flex gap-4">
+              <Search/>
+              <input onChange={(e) => setQuery(e.target.value)} type="text" className="outline-none" placeholder="Search name or URL"/>
+            </div>
+            <ListFilter/>
+          </div>
+  
+          <div className="flex flex-col gap-2">
+            {slugs?.map((slug) => ( 
+              <div key={slug.id} className="p-4 bg-white rounded-lg flex justify-between items-center">
+                <div className="max-w-lg">
+                  <div className="flex gap-2 text-blue-800">
+                    <Link2/>
+                    <span className="font-bold">{window.location.host}/{slug.slug}</span>
+                  </div>
+                  <p title={slug.original_url} className="max-w-2xl truncate">{slug.original_url}</p>
+                </div>
+                <div className="flex gap-4 items-center">
+                  <div className="bg-blue-300/50 p-1 rounded-md">
+                    <Copy size={18} />
+                  </div>
+                  <button onClick={(e) => {
+                    handleRemove(slug.id)
+                  }}>
+                    <Trash size={18}/>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
