7fffede035bac123ee544f122938229d0931cfb3
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-19 11:08:53 +0700

    feat: add dashboard router create, my-linkk, profile

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import RegisterPage from "../pages/auth/register";
 import LoginPage from "../pages/auth/login";
+import CreateShortLink from "../pages/dahsboard/create";
+import RedirectPage from "../pages/redirect";
+import HomePage from "../pages/home";
+import MyLinksPage from "../pages/dahsboard/my-link";
+import ProfilePage from "../pages/dahsboard/profile";
 
 export const router = createBrowserRouter([
   {
@@ -16,4 +21,29 @@ export const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path: "/dashboard", 
+    children: [ 
+      {
+        path: "create", 
+        element: <CreateShortLink/>
+      }, 
+      { 
+        path: "my-link", 
+        element: <MyLinksPage/>
+      },
+      { 
+        path: "profile",
+        element: <ProfilePage/>
+      }
+    ]
+  },
+  { 
+    path: "/:slug",
+    element: <RedirectPage/>
+  },
+  { 
+    path: "/",
+    element: <HomePage/>
+  }
 ]);
