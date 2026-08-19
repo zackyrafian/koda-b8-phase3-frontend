6d5a200bcc374c8f59fbefcfdbaaa5a79f9c9568
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-08-20 05:00:08 +0700

    feat: setup global Toaster with react-hot-toast

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,12 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.css'
+import { Toaster } from "react-hot-toast"
 import { router } from './router';
 import { AuthProvider } from '../context/authContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster
+        position='top-right'
+        toastOptions={{
+          duration: 2000, 
+          style: { 
+            border: '10px', 
+            fontSize: '12px'
+          }
+        }}
+      />
       <RouterProvider router={router}/>
     </AuthProvider>
   </StrictMode>,
